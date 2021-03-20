521cc46d1558648b17ec20b9158d73011c7e4b36
bhaskaragautama <bhaskaragautama@gmail.com>
2021-03-20 11:37:19 +0800

    home

--- assets/js/ceck.js
+++ assets/js/ceck.js
@@ -1,11 +1,10 @@
 var cook = document.cookie;
 var ck = cook.split(';');
 ck.forEach(function(idx, el) {
-   console.log(el);
-   // hooman = el.split('=');
-   // if(hooman[0] == 'nim') {
-   //    if(hoom.indexOf(md5(hooman[1])) < 0) {
-   //       window.location.href='index.html';
-   //    }
-   // }
+   hooman = toString(el).split('=');
+   if(hooman[0] == 'nim') {
+      if(hoom.indexOf(md5(hooman[1])) < 0) {
+         window.location.href='index.html';
+      }
+   }
 });
