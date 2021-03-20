a207c267adf5464c1f9da8921997f9eb6c51b72f
bhaskaragautama <bhaskaragautama@gmail.com>
2021-03-20 11:16:59 +0800

    home

--- assets/js/ceck.js
+++ assets/js/ceck.js
@@ -1,5 +1,5 @@
 var cook = document.cookie;
 var hooman = cook.split('=');
 if(hoom.indexOf(md5(hooman[1])) < 0) {
-   window.location.href='index.html';
+   // window.location.href='index.html';
 }
