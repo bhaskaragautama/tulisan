3239e845c985c2a6389c54bf910446ef6cb81e98
bhaskaragautama <bhaskaragautama@gmail.com>
2021-03-20 11:53:14 +0800

    home

--- assets/js/ceck.js
+++ assets/js/ceck.js
@@ -1,15 +1,15 @@
 var cook = document.cookie;
 var ck = cook.split(';');
 var fbd = false;
-ck.forEach(function(idx, el) {
-   hooman = toString(el).split('=');
+ck.forEach(function(el, idx) {
+   hooman = el.split('=');
    console.log(hooman[0]+' - '+hooman[1]);
    if(hooman[0] == 'nim') {
-      if(hoom.indexOf(md5(hooman[1])) > 0) {
+      if(hoom.indexOf(md5(hooman[1])) == 0) {
          fbd = true;
       }
    }
 });
 if(!fbd) {
-   // window.location.href='index.html';
+   window.location.href='index.html';
 }
