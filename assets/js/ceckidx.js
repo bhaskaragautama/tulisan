c78d10aecc32ea00a79a64a837640c17f4b1fa8a
bhaskaragautama <bhaskaragautama@gmail.com>
2021-03-20 17:02:21 +0800

    add user

--- assets/js/ceckidx.js
+++ assets/js/ceckidx.js
@@ -7,7 +7,6 @@ var ck = cook.split(';');
 var fbd = false;
 ck.forEach(function(el, idx) {
    hooman = el.split('=');
-   console.log(hooman[0]+' - '+hooman[1]);
    if(hooman[0] == 'nim') {
       jQexec(hooman[1]);
    }
