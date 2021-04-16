35d8eff4be7e6f62f2c10e5b3ec0257979eb36ce
BG <bhaskaragautama@gmail.com>
2021-04-16 09:21:59 +0800

    add user

--- assets/js/hooman-rights.js
+++ assets/js/hooman-rights.js
@@ -21,5 +21,6 @@ var hoom = [
    '78cd65bbacfb992dc29196778d5088ce',
    'efb2a6a210eaa220798689d60018d04c',
    '0ea1521b0866f6c13c30e98a5ffe06d0',
-   '1bdc81643cf6d8ae3dfd75c375697f07'
+   '1bdc81643cf6d8ae3dfd75c375697f07',
+   '14d874b1ab56d567ae4bfb4e63e10a0a'
 ];
